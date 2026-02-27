b0fbac5ed4096bee862b566ef850dbd786f027e5
r3vn <giuseppe.corti@protonmail.com>
2026-02-27 10:38:46 +0100

    [admin] added export_csv_action

--- src/lazycommons/admin.py
+++ src/lazycommons/admin.py
@@ -1,8 +1,11 @@
 import json
-
+import csv
+from django.db import models
 from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
+from django.http import HttpResponse
+
 
 @admin.action(description="Duplicate selected Item(s)")
 def duplicate_action(modeladmin, request, queryset):
@@ -12,6 +15,54 @@ def duplicate_action(modeladmin, request, queryset):
     for object in queryset:
         object.id = None
         object.save()
+
+@admin.action(description="Export selected objects as CSV")
+def export_csv_action(modeladmin, request, queryset):
+    """
+    Generic CSV export admin action.
+    """
+    model = queryset.model
+    meta = model._meta
+
+    # Only export concrete fields (skip reverse relations)
+    fields = [field for field in meta.fields]
+
+    response = HttpResponse(content_type="text/csv")
+    response["Content-Disposition"] = f'attachment; filename="{meta.model_name}.csv"'
+
+    writer = csv.writer(response)
+
+    # Header row
+    writer.writerow([field.name for field in fields])
+
+    for obj in queryset:
+        row = []
+
+        for field in fields:
+            value = None
+
+            # Choices: use get_FOO_display()
+            if field.choices:
+                display_method = "get_%s_display" % field.name
+                if hasattr(obj, display_method):
+                    value = getattr(obj, display_method)()
+                else:
+                    value = getattr(obj, field.name)
+
+            # ForeignKey: resolve via __str__()
+            elif isinstance(field, models.ForeignKey):
+                fk_obj = getattr(obj, field.name)
+                value = str(fk_obj) if fk_obj else ""
+
+            # Default field
+            else:
+                value = getattr(obj, field.name)
+
+            row.append(value)
+        writer.writerow(row)
+
+    return response
+
 
 def linkify(field_path: str) -> str:
     """
@@ -41,6 +92,7 @@ def linkify(field_path: str) -> str:
 
     _linkify.short_description = field_path.replace('.', ' -> ')  # Sets column name
     return _linkify
+
 
 # https://stackoverflow.com/a/72256767
 class PrettyJSONEncoder(json.JSONEncoder):
